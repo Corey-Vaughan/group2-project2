ddb07424c7042e45b13156cf2e110269d9ee046e
Peter Norman <october8th@hotmail.com>
2018-02-23 22:06:17 -0500

    afterupload

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.listen(process.env.PORT || 3000);
  * Configure the AWS region of the target bucket.
  * Remember to change this to the relevant region.
  */
-aws.config.region = 'us-east-1';
+//aws.config.region = 'us-east-1';
 
 /*
  * Load the S3 information from the environment variables.
@@ -38,7 +38,7 @@ const S3_BUCKET = process.env.S3_BUCKET;
  * Respond to GET requests to /account.
  * Upon request, render the 'account.html' web page in views/ directory.
  */
-app.get('/', (req, res) => res.render('account.html'));
+app.get('/account', (req, res) => res.render('account.html'));
 
 /*
  * Respond to GET requests to /sign-s3.
@@ -78,4 +78,5 @@ app.get('/sign-s3', (req, res) => {
  */
 app.post('/save-details', (req, res) => {
   // TODO: Read POSTed form data and do something useful
+  res.render('account.html');
 });
