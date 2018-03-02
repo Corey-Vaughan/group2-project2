594936fa84807492585d185e5a17c5573f2d506b
Corey-Vaughan <coreypvaughan@gmail.com>
2018-03-02 18:20:37 -0500

    remove broken authentication

--- server.js
+++ server.js
@@ -53,34 +53,3 @@ db.sequelize.sync({ force: false }).then(function() {
   });
 });
 
-// User Authentication Configuration
-// =============================================================
-require('./config/passport')(passport); // pass passport for configuration
-
-
-
-
-// set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-// app.use(bodyParser.urlencoded({
-// 	extended: true
-// }));
-
-// required for passport
-app.use(session({
-	secret: 'wearethebestgroup',
-	resave: true,
-	saveUninitialized: true
- } )); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
-
-
-// routes ======================================================================
-require('./routes/user-routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
-
-// launch ======================================================================
-// app.listen(port);
-// console.log('The magic happens on port ' + port);
