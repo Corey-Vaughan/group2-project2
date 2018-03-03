a39c6d092ddc96df55f85b04d190ffb80ce7cd90
John Rodriguez <johnnyrod92@gmail.com>
2018-03-02 19:21:44 -0500

    updated modal

--- public/js/map.js
+++ public/js/map.js
@@ -53,7 +53,7 @@ function getRecipient(){
   return $(".bookit").attr("data-name")
 }
 
-  $('#bookit').click(function (event) {
+  $('.bookit').click(function (event) {
   var button = $(this); // Button that triggered the modal#
   var recipient = getRecipient();
   var emailTitle =  $('"[name="start-date"') + " - " +$('"[name="end-date"');
@@ -62,7 +62,7 @@ function getRecipient(){
    // Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $("#bookIt");
+  var modal = $(".bookIt");
   modal.find('#uniqueId').text('New message to ' + recipient)
   modal.find('#title-text').text(emailTitle)
   modal.find('.modal-body input').val(recipient)
